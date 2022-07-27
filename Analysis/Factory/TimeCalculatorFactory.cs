de834ca4340ab9602b788ca7531b56faa93d56f1
ITBoar <6986789+ITBoar@users.noreply.github.com>
2022-07-27 18:02:31 +0200

    Fix issue where break calculator was created even when break was set to zero

--- Analysis/Factory/TimeCalculatorFactory.cs
+++ Analysis/Factory/TimeCalculatorFactory.cs
@@ -8,7 +8,7 @@ internal class TimeCalculatorFactory
 {
     public static TimeCalculator CreateCalculator(Settings settings)
     {
-        if (settings.NrbOfMinutesBreakPerHour is >= 0 and < 60)
+        if (settings.NrbOfMinutesBreakPerHour is > 0 and < 60)
             return new WorkWithBreakCalculator(TimeSpan.FromMinutes(settings.NrbOfMinutesBreakPerHour));
         return new WorkTimeCalculator();
     }
